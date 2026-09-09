5487ca5b9e13dc9c8e68415d5fea17ca518f43a1
Henrique <riquesoares2007@gmail.com>
2026-09-08 21:15:06 -0300

    fix:comenta resetar

--- backend/resetar.py
+++ backend/resetar.py
@@ -1,9 +1,11 @@
 #import sqlite3
 
 #conexao = sqlite3.connect("dados.db")
+#conexao.execute("DELETE FROM produtos")
+#conexao.execute("DELETE FROM usuarios")
 #conexao.execute("DELETE FROM sqlite_sequence WHERE name = 'usuarios'")
 #conexao.execute("DELETE FROM sqlite_sequence WHERE name = 'produtos'")
 #conexao.commit()
 #conexao.close()
 
-#print("Contadores resetados!") 
+#print("Todos os registros foram apagados e os contadores resetados!")
